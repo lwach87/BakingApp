40bbd27ccb45839d2690951fe11122473a4ecb8a
Łukasz Wachowski <lwach87@gmail.com>
2019-03-28 16:34:01 +0100

    Navigation added

--- app/src/main/java/com/example/lukaszwachowski/bakingapp/ui/movie/MovieActivity.java
+++ app/src/main/java/com/example/lukaszwachowski/bakingapp/ui/movie/MovieActivity.java
@@ -1,52 +1,51 @@
 package com.example.lukaszwachowski.bakingapp.ui.movie;
 
-import static com.example.lukaszwachowski.bakingapp.configuration.Constants.POSITION;
-import static com.example.lukaszwachowski.bakingapp.configuration.Constants.STEPS_LIST;
-
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+
 import com.example.lukaszwachowski.bakingapp.R;
 import com.example.lukaszwachowski.bakingapp.network.model.Step;
-import dagger.android.AndroidInjection;
-import java.util.ArrayList;
+import com.google.android.material.tabs.TabLayout;
+
 import javax.inject.Inject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dagger.android.AndroidInjection;
 
 public class MovieActivity extends AppCompatActivity {
 
-  @BindView(R.id.movie_pager)
-  ViewPager moviePager;
+    @BindView(R.id.movie_pager)
+    ViewPager moviePager;
 
-  @BindView(R.id.tab_layout)
-  TabLayout tabLayout;
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
 
-  @BindView(R.id.toolbar)
-  Toolbar toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
-  @Inject
-  PagerAdapter pagerAdapter;
+    @Inject
+    PagerAdapter pagerAdapter;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_movie);
-    ButterKnife.bind(this);
-    setSupportActionBar(toolbar);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
 
-    toolbar.setNavigationOnClickListener(v -> onBackPressed());
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
-    AndroidInjection.inject(this);
+        AndroidInjection.inject(this);
 
-    ArrayList<Step> steps = getIntent().getExtras().getParcelableArrayList(STEPS_LIST);
-    int position = getIntent().getExtras().getInt(POSITION);
+        Step[] steps = MovieActivityArgs.fromBundle(getIntent().getExtras()).getSteps();
+        int position = MovieActivityArgs.fromBundle(getIntent().getExtras()).getPosition();
 
-    pagerAdapter.swapData(steps);
-    moviePager.setAdapter(pagerAdapter);
-    moviePager.setCurrentItem(position);
-    tabLayout.setupWithViewPager(moviePager);
-  }
+        pagerAdapter.swapData(steps);
+        moviePager.setAdapter(pagerAdapter);
+        moviePager.setCurrentItem(position);
+        tabLayout.setupWithViewPager(moviePager);
+    }
 }
--- app/src/main/java/com/example/lukaszwachowski/bakingapp/ui/movie/PagerAdapter.java
+++ app/src/main/java/com/example/lukaszwachowski/bakingapp/ui/movie/PagerAdapter.java
@@ -1,48 +1,53 @@
 package com.example.lukaszwachowski.bakingapp.ui.movie;
 
 import android.content.Context;
+
+import com.example.lukaszwachowski.bakingapp.R;
+import com.example.lukaszwachowski.bakingapp.fragments.MovieDetailsFragment.MovieDetailsFragment;
+import com.example.lukaszwachowski.bakingapp.network.model.Step;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import com.example.lukaszwachowski.bakingapp.R;
-import com.example.lukaszwachowski.bakingapp.fragments.MovieDetailsFragment.MovieDetailsFragment;
-import com.example.lukaszwachowski.bakingapp.network.model.Step;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PagerAdapter extends FragmentPagerAdapter {
 
-  private ArrayList<Step> steps = new ArrayList<>();
-  private Context context;
+    private ArrayList<Step> steps = new ArrayList<>();
+    private Context context;
 
-  public PagerAdapter(FragmentManager fm, Context context) {
-    super(fm);
-    this.context = context;
-  }
-
-  @Override
-  public Fragment getItem(int position) {
-    return MovieDetailsFragment.newInstance(steps, position);
-  }
-
-  @Override
-  public int getCount() {
-    return steps.size();
-  }
-
-  @Nullable
-  @Override
-  public CharSequence getPageTitle(int position) {
-    switch (position) {
-      case 0:
-        return steps.get(position).shortDescription;
-      default:
-        return context.getString(R.string.step) + " " + position;
+    public PagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
     }
-  }
 
-  public void swapData(List<Step> newSteps) {
-    steps.addAll(newSteps);
-  }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return MovieDetailsFragment.newInstance(steps, position);
+    }
+
+    @Override
+    public int getCount() {
+        return steps.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return steps.get(position).shortDescription;
+            default:
+                return context.getString(R.string.step) + " " + position;
+        }
+    }
+
+    public void swapData(Step[] newSteps) {
+        steps.addAll(Arrays.asList(newSteps));
+    }
 }
